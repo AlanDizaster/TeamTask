dce32873fa9a1a8bda145cacee45c863f670e255
Anton Shatunov <shatunov00@gmail.com>
2020-12-26 17:58:11 +0300

    Добавлен GetFunc.h

---
+++ TeamTask/GetFunc.h
@@ -0,0 +1,8 @@
+#ifndef GETFUNC_H   
+#define GETFUNC_H   
+#include <string> 
+
+using namespace std;
+void getaLine(string& inStr); //функция для ввода строки с ограничением ввода
+char getaChar();    //функция для ввода символов с ограничением ввода                  
+#endif   
