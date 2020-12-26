30f127214c5676302beb896cb22d5e5796dc53d7
Anton Shatunov <shatunov00@gmail.com>
2020-12-26 15:20:21 +0300

    добавлен Authentification.h

---
+++ TeamTask/Authenyification.h
@@ -0,0 +1,20 @@
+#ifndef AUTHENTIFICATION_H   
+#define AUTHENTIFICATION_H 
+#include "Profile.h"
+
+class Authentication //экран ввода пользователя
+{
+private:
+	Profile* ptrProfile; //указатель на пользователя
+	string addLogin;      //логин
+	string addPassword;   //пароль
+	int rules;  //права доступа
+public:
+	Authentication(Profile* ptrUL) : ptrProfile(ptrUL)
+	{
+	}
+	int getRules(); //получить номер права доступа
+	void setUser(); //ввод пользователя
+};
+
+#endif   
