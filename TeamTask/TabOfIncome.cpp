4ce272c3c2d3abbd4cd50f72734f7ace87de87a7
Fedotov Vladimir <adidasop@yandex.ru>
2020-12-26 19:01:38 +0300

    Добавлен файл реализации для Таблицы доходов

---
+++ TeamTask/TabOfIncome.cpp
@@ -0,0 +1,52 @@
+#include "stdafx.h" 
+#include "TabOfIncome.h" 
+using namespace std;
+
+TabOfIncome::~TabOfIncome()
+{
+	while (!setPtrsRR.empty())
+	{
+		iter = setPtrsRR.begin();
+		delete *iter;
+		setPtrsRR.erase(iter);
+	}
+}
+
+
+void TabOfIncome::insertIncome(Income* ptrRow) {
+	setPtrsRR.push_back(ptrRow);
+}
+
+
+
+void TabOfIncome::display()       //отоброжение на экране доходов                  
+{
+
+	cout << "\nФИО Клиента        |Стоимость работы        |Стоимость материалов        |Сумма              |Дата оплаты\n";
+	cout << "----------------------------------------------------------------------------------------------------------------------\n";
+	if (setPtrsRR.empty())
+		cout << "---Нет доходов---\n" << endl;
+	else
+	{
+		iter = setPtrsRR.begin();
+		while (iter != setPtrsRR.end())
+		{
+
+			cout << std::setw(15) << (*iter)->getName() << "    |     " << std::setw(15) << (*iter)->getPriceService() << "    |     " << std::setw(15) << (*iter)->getPriceMaterials() << "    |     " << std::setw(15) << (*iter)->getPriceMaterials() + (*iter)->getPriceService() << "    |     " << std::setw(15) << (*iter)->getDate() << endl;
+			*iter++;
+		}
+	}
+}
+
+float TabOfIncome::getSumOfIncomes()  //общая сумма доходов
+{
+	float sumProfits = 0.0;
+	iter = setPtrsRR.begin();
+	while (iter != setPtrsRR.end())
+	{
+		sumProfits += (*iter)->getPriceMaterials() + (*iter)->getPriceService();
+		iter++;
+	}
+	return sumProfits;
+}
+
